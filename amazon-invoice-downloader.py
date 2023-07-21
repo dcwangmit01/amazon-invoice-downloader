f98e6c4ee4aef4f07e3ca09df91a75c7d56265bb
David C Wang <dcwangmit01@gmail.com>
2023-07-20 21:13:25 -0700

    Rewrite to use Playwright instead of Selenium

--- amazon-invoice-downloader.py
+++ amazon-invoice-downloader.py
@@ -15,14 +15,11 @@ Options:
   -h --help     Show this screen.
 """
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
+from playwright.sync_api import sync_playwright, TimeoutError
+from datetime import datetime
 import random
 import time
 import os
-import json
 from docopt import docopt
 
 
@@ -30,119 +27,108 @@ def sleep():
     # Add human latency
 
     # Generate a random sleep time between 3 and 5 seconds
-    sleep_time = random.uniform(3, 5)
+    sleep_time = random.uniform(2, 5)
     # Sleep for the generated time
     time.sleep(sleep_time)
 
 
-def amazon_invoice_downloader(email, password, num_receipts):
+def amazon_invoice_downloader(playwright, email, password, num_receipts):
     # Ensure the location exists for where we will save our downloads
     target_dir = os.getcwd() + "/" + "downloads"
     os.makedirs(target_dir, exist_ok=True)
 
-    # Create chrome options for saving pages as PDF
-    options = webdriver.ChromeOptions()
-    settings = {
-        "recentDestinations": [
-            {
-                "id": "Save as PDF",
-                "origin": "local",
-                "account": "",
-            }
-        ],
-        "selectedDestinationId": "Save as PDF",
-        "version": 2,
-    }
+    # Create Playwright context with Chromium
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
 
-    prefs = {
-        "printing.print_preview_sticky_settings.appState": json.dumps(settings),
-        "savefile.default_directory": target_dir,
-    }
+    page = context.new_page()
+    # page.set_default_timeout(10000)
+    page.goto("https://www.amazon.com/")
+    page.get_by_role("link", name="Sign in", exact=True).click()
 
-    options.add_experimental_option("prefs", prefs)
-    options.add_argument("--kiosk-printing")
+    page.get_by_label("Email").click()
+    page.get_by_label("Email").fill(email)
+    page.get_by_role("button", name="Continue").click()
 
-    # Initiate the browser
-    driver = webdriver.Chrome(options=options)
+    page.get_by_label("Password").click()
+    page.get_by_label("Password").fill(password)
+    page.get_by_label("Keep me signed in").check()
+    page.get_by_role("button", name="Sign in").click()
 
-    # Open Amazon website
-    driver.get("https://www.amazon.com")
-    sleep()
-
-    # Click sign in button
-    signin_elem = driver.find_element(By.PARTIAL_LINK_TEXT, "sign in")
-    signin_elem.click()
-    sleep()
-
-    # Enter and submit email
-    email_elem = driver.find_element(By.NAME, "email")
-    email_elem.send_keys(email)
-
-    continue_elem = driver.find_element(By.ID, "continue")
-    continue_elem.click()
-    sleep()
-
-    # Enter and submit password
-    password_elem = driver.find_element(By.NAME, "password")
-    password_elem.send_keys(password)
-
-    submit_elem = driver.find_element(By.ID, "signInSubmit")
-    submit_elem.click()
-    sleep()
-
-    # Click the orders button
-    orders_elem = driver.find_element(By.PARTIAL_LINK_TEXT, "Orders")
-    orders_elem.click()
+    page.get_by_role("link", name="Returns & Orders").click()
     sleep()
 
     # Get a list of years from the select options
-    select = Select(driver.find_element(By.NAME, "orderFilter"))
-    years = [i.text for i in select.options[2:]]  # skip the first two text options
+    select = page.query_selector('select[name="orderFilter"]')
+    years = select.inner_text().split("\n")[2:]  # skip the first two text options
 
     # Year Loop
     save_count = 0
-    for year_select_index in range(2, len(years) + 2):
+    for year in years:
         if save_count >= num_receipts:
             break
 
         # Select the right year in the order filter
-        Select(driver.find_element(By.NAME, "orderFilter")).select_by_index(year_select_index)
+        page.locator("#a-autoid-1-announce").click()  # Time Range Dropdown Filter
+        page.get_by_role("option", name=year).click()  # Select the year (descending order, most recent first)
         sleep()
 
         # Pagination Loop
-        while True and save_count <= num_receipts:
-            num_invoices_on_page = len(driver.find_elements(By.PARTIAL_LINK_TEXT, "View invoice"))
-            # Invoice Loop
-            for i in range(num_invoices_on_page):
-                invoice_elem = driver.find_elements(By.PARTIAL_LINK_TEXT, "View invoice")[i]
-                invoice_elem.click()
-                sleep()
-                driver.execute_script("window.print();")
+        pagination = 0
+        while save_count < num_receipts:
+            # Order Loop
+            order_cards = page.query_selector_all(".js-order-card")
+            for order_card in order_cards:
+                if save_count >= num_receipts:
+                    break
+
+                print(f"year{year} pagination{pagination} order{save_count}")
+                spans = order_card.query_selector_all("span")
+
+                date = datetime.strptime(spans[1].inner_text(), "%B %d, %Y").strftime("%Y%m%d")
+                total = spans[3].inner_text().replace("$", "").replace(",", "")  # remove dollar sign and commas
+                orderid = spans[9].inner_text()
+
+                file_name = f"{target_dir}/{date}_{total}_amazon_{orderid}.pdf"
+
+                if os.path.isfile(file_name):
+                    print(f"File [{file_name}] already exists")
+                else:
+                    print(f"Saving file [{file_name}]")
+                    # Save
+                    link = "https://www.amazon.com/" + order_card.query_selector(
+                        'xpath=//a[contains(text(), "View invoice")]'
+                    ).get_attribute("href")
+                    invoice_page = context.new_page()
+                    invoice_page.goto(link)
+                    invoice_page.pdf(
+                        path=file_name,
+                        format="Letter",
+                        margin={"top": ".5in", "right": ".5in", "bottom": ".5in", "left": ".5in"},
+                    )
+                    invoice_page.close()
                 save_count += 1
-                driver.back()
-                sleep()
 
-            if save_count > num_receipts:
-                break
-
-            # Check if there is a next
-            if num_invoices_on_page < 10:
-                # Then there is not a next page pagination of receipts
-                break
+            pagination += 1
 
             # Go to the next page pagination, and continue downloading
             #   if there is not a next page then break
             try:
-                next_elem = driver.find_element(By.PARTIAL_LINK_TEXT, "Next")
-                next_elem.click()
-            except NoSuchElementException:
+                page.get_by_role("link", name="Next →").click()
+                sleep()
+            except TimeoutError:
+                print("Timeout Error")
                 break
 
-    # Close the driver
-    driver.quit()
+    # Close the browser
+    context.close()
+    browser.close()
 
 
 if __name__ == "__main__":
     arguments = docopt(__doc__)
 
-    amazon_invoice_downloader(arguments["<email>"], arguments["<password>"], int(arguments["<num_receipts>"]))
+    with sync_playwright() as playwright:
+        amazon_invoice_downloader(
+            playwright, arguments["<email>"], arguments["<password>"], int(arguments["<num_receipts>"])
+        )
