69b64b2687ffb65f262e642f8fe72cd06d5fa5ad
David C Wang <dcwangmit01@gmail.com>
2023-08-04 22:45:01 -0700

    Base fetch bounds on date range

--- Makefile
+++ Makefile
@@ -5,14 +5,15 @@ UNAME_S := $(shell uname -s)
 PYENV_NAME := $(notdir $(CURDIR))
 PYTHON_VERSION := 3.11.2
 
-
 PYTHON_PACKAGES := \
 	pipreqs black flake8 \
 	playwright
 
+PYTHON_FILES = $(shell find * -type f -name *.py)
+
 .PHONY: format
 format: deps  ## Auto-format and check pep8
-	  black --line-length 119 *.py && flake8 --max-line-length=119 *.py
+	  black --line-length 119 $(PYTHON_FILES) && flake8 --max-line-length=119 $(PYTHON_FILES)
 
 deps:  ## Ensure OS Dependencies (Only works for MacOS)
 ifeq ($(UNAME_S),Darwin)
@@ -38,10 +39,21 @@ pyenv: deps  ## Create the pyenv for Python development
 	done
 	python -m pip install --upgrade pip
 
-clean:
+run:  ## Run a few examples
+	hatch run amazon-invoice-downloader --year 2022
+	hatch run amazon-invoice-downloader --date-range 20230131-20221201
+	hatch run amazon-invoice-downloader --email=user@domain.com --password=mypassword
+
+build:  ## Build the project
+	hatch build
+
+clean:  ## Clean the project
+	hatch clean
+
+mrclean: clean  ## Really clean the project and all downloads
 	rm -rf downloads/
 
-help: ## Print list of Makefile targets
+help:  ## Print list of Makefile targets
 	@# Taken from https://github.com/spf13/hugo/blob/master/Makefile
 	@grep --with-filename -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | \
 	  cut -d ":" -f2- | \
--- src/amazon_invoice_downloader/cli/__init__.py
+++ src/amazon_invoice_downloader/cli/__init__.py
@@ -2,19 +2,32 @@
 #
 # SPDX-License-Identifier: MIT
 
-"""Amazon Invoice Downloader
+"""
+Amazon Invoice Downloader
 
 Usage:
-  amazon-invoice-downloader.py <email> <password> <num_receipts>
+  amazon-invoice-downloader.py \
+    [--email=<email> --password=<password>] \
+    [--year=<YYYY> | --date-range=<YYYYMMDD-YYYYMMDD>]
   amazon-invoice-downloader.py (-h | --help)
 
-Arguments:
-  email          Amazon login email.
-  password       Amazon login password.
-  num_receipts   The number of most recent receipts to download.
+Login Options:
+  --email=<email>          Amazon login email  [default: $AMAZON_EMAIL].
+  --password=<password>    Amazon login password  [default: $AMAZON_PASSWORD].
+
+Date Range Options:
+  --date-range=<YYYYMMDD-YYYYMMDD>  Start and end date range
+  --year=<YYYY>            Year, formatted as YYYY  [default: <CUR_YEAR>].
 
 Options:
-  -h --help     Show this screen.
+  -h --help                Show this screen.
+
+Examples:
+  amazon-invoice-downloader.py --year=2022  # This uses env vars $AMAZON_EMAIL and $AMAZON_PASSWORD
+  amazon-invoice-downloader.py --date-range=20220101-20221231
+  amazon-invoice-downloader.py --email=user@example.com --password=secret  # Defaults to current year
+  amazon-invoice-downloader.py --email=user@example.com --password=secret --year=2022
+  amazon-invoice-downloader.py --email=user@example.com --password=secret --date-range=20220101-20221231
 """
 
 from amazon_invoice_downloader.__about__ import __version__
@@ -24,19 +37,47 @@ from datetime import datetime
 import random
 import time
 import os
+import sys
 from docopt import docopt
 
 
 def sleep():
     # Add human latency
-
     # Generate a random sleep time between 3 and 5 seconds
     sleep_time = random.uniform(2, 5)
     # Sleep for the generated time
     time.sleep(sleep_time)
 
 
-def amazon_invoice_downloader(playwright, email, password, num_receipts):
+def run(playwright, args):
+    email = args.get('--email')
+    if email == '$AMAZON_EMAIL':
+        email = os.environ.get('AMAZON_EMAIL')
+    if not email:
+        print("Email must be specified")
+        sys.exit(1)
+
+    password = args.get('--password')
+    if password == '$AMAZON_PASSWORD':
+        password = os.environ.get('AMAZON_PASSWORD')
+    if not password:
+        print("Password must be specified")
+        sys.exit(1)
+
+    # Parse date ranges int start_date and end_date
+    if args['--date-range']:
+        start_date, end_date = args['--date-range'].split('-')
+    elif args['--year'] != "<CUR_YEAR>":
+        start_date, end_date = args['--year'] + "0101", args['--year'] + "1231"
+    else:
+        year = str(datetime.now().year)
+        start_date, end_date = year + "0101", year + "1231"
+    start_date = datetime.strptime(start_date, "%Y%m%d")
+    end_date = datetime.strptime(end_date, "%Y%m%d")
+
+    # Debug
+    # print(email, password, start_date, end_date)
+
     # Ensure the location exists for where we will save our downloads
     target_dir = os.getcwd() + "/" + "downloads"
     os.makedirs(target_dir, exist_ok=True)
@@ -64,36 +105,54 @@ def amazon_invoice_downloader(playwright, email, password, num_receipts):
 
     # Get a list of years from the select options
     select = page.query_selector('select[name="orderFilter"]')
-    years = select.inner_text().split("\n")[2:]  # skip the first two text options
+    years = select.inner_text().split("\n")  # skip the first two text options
 
-    # Year Loop
-    save_count = 0
+    # Filter years to include only numerical years (YYYY)
+    years = [year for year in years if year.isnumeric()]
+
+    # Filter years to the include only the years between start_date and end_date inclusively
+    years = [year for year in years if start_date.year <= int(year) <= end_date.year]
+    years.sort(reverse=True)
+
+    # Year Loop (Run backwards through the time range from years to pages to orders)
     for year in years:
-        if save_count >= num_receipts:
-            break
-
-        # Select the right year in the order filter
+        # Select the year in the order filter
         page.locator("#a-autoid-1-announce").click()  # Time Range Dropdown Filter
         page.get_by_role("option", name=year).click()  # Select the year (descending order, most recent first)
         sleep()
 
-        # Pagination Loop
-        pagination = 0
-        while save_count < num_receipts:
+        # Page Loop
+        first_page = True
+        done = False
+        while not done:
+            # Go to the next page pagination, and continue downloading
+            #   if there is not a next page then break
+            try:
+                if first_page:
+                    first_page = False
+                else:
+                    page.get_by_role("link", name="Next →").click()
+                sleep()  # sleep after every page load
+            except TimeoutError:
+                # There are no more pages
+                break
+
             # Order Loop
             order_cards = page.query_selector_all(".js-order-card")
             for order_card in order_cards:
-                if save_count >= num_receipts:
-                    break
-
-                print(f"year{year} pagination{pagination} order{save_count}")
+                # Parse the order card to create the date and file_name
                 spans = order_card.query_selector_all("span")
-
-                date = datetime.strptime(spans[1].inner_text(), "%B %d, %Y").strftime("%Y%m%d")
+                date = datetime.strptime(spans[1].inner_text(), "%B %d, %Y")
                 total = spans[3].inner_text().replace("$", "").replace(",", "")  # remove dollar sign and commas
                 orderid = spans[9].inner_text()
+                date_str = date.strftime("%Y%m%d")
+                file_name = f"{target_dir}/{date_str}_{total}_amazon_{orderid}.pdf"
 
-                file_name = f"{target_dir}/{date}_{total}_amazon_{orderid}.pdf"
+                if date > end_date:
+                    continue
+                elif date < start_date:
+                    done = True
+                    break
 
                 if os.path.isfile(file_name):
                     print(f"File [{file_name}] already exists")
@@ -111,29 +170,14 @@ def amazon_invoice_downloader(playwright, email, password, num_receipts):
                         margin={"top": ".5in", "right": ".5in", "bottom": ".5in", "left": ".5in"},
                     )
                     invoice_page.close()
-                save_count += 1
-
-            pagination += 1
-
-            # Go to the next page pagination, and continue downloading
-            #   if there is not a next page then break
-            try:
-                page.get_by_role("link", name="Next →").click()
-                sleep()
-            except TimeoutError:
-                print("Timeout Error")
-                break
 
     # Close the browser
     context.close()
     browser.close()
 
 
-
 def amazon_invoice_downloader():
-    arguments = docopt(__doc__)
+    args = docopt(__doc__)
 
     with sync_playwright() as playwright:
-        amazon_invoice_downloader(
-            playwright, arguments["<email>"], arguments["<password>"], int(arguments["<num_receipts>"])
-        )
+        run(playwright, args)
